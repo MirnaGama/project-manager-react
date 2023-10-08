f0f0f04f3beda2ad2c41f5c1c77bf0a09e7bb4fd
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-10-08 19:15:15 -0300

    Include routes for initial pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import './App.css';
-function App() {
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+import Home from "./components/page/Home";
+import CreateProject from "./components/page/CreateProject";
+import NotFound from "./components/page/error/NotFound";
+
+
+function App() {
   return (
-   <div>Hello world!</div>
+
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/create-project">Create a New Project</Link>
+      </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/create-project" element={<CreateProject/>} />
+        {/* Error route */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <footer>Footer</footer>
+    </Router>
   );
 }
 
