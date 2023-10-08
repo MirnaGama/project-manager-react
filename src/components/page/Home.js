f9ab1323e1ed078a795515445309262d7bba5555
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-10-08 20:45:12 -0300

    Create initial homepage

--- src/components/page/Home.js
+++ src/components/page/Home.js
@@ -1,3 +1,11 @@
+import "./Home.modules.css";
+import savings from '../../img/savings.svg';
+
 export default function Home() {
-    return (<><p>PLACEHOLDER</p></>)
+    return (<section className="home_container">
+        <h1>Welcome to <span>Project Manager</span></h1>
+        <p>Start right now</p>
+        <a href="/create-project">Create a New Project</a>
+        <img src={savings} alt="savings"/>
+    </section>)
 }
