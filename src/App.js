49d9b0b27efa6de0dd13fc87ed4f0516865c9a66
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-10-12 18:28:01 -0300

    Add toast container for success and error messages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./components/page/Home";
 import CreateProject from "./components/page/CreateProject";
+import ListProjects from "./components/page/ListProjects";
 import NotFound from "./components/page/error/NotFound";
 
 import Container from "./components/layout/Container";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
@@ -18,10 +20,12 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/create-project" element={<CreateProject/>} />
+        <Route exact path="/my-projects" element={<ListProjects/>} />
         {/* Error route */}
         <Route path="*" element={<NotFound />} />
       </Routes>
       </Container>
+      <ToastContainer/>
       <Footer/>
     </Router>
   );
--- src/components/page/CreateProject.js
+++ src/components/page/CreateProject.js
@@ -2,24 +2,27 @@ import "./CreateProject.modules.css";
 import "bootstrap/dist/css/bootstrap.css";
 import baseConnection from "../../config/baseConnection";
 import ProjectForm from "../layout/ProjectForm";
+import { sendErrorToast, sendSuccessToast } from "../util/Toast";
 
 export default function CreateProject() {
-
   const createNewProject = (project) => {
     project.cost = 0;
     project.services = [];
 
-    baseConnection.post('/projects', project).then(
-      (response) => {
-       // message here
-      }
-    ).catch(err => console.log(err));
-  }
+    baseConnection
+      .post("/projects", project)
+      .then((response) => {
+        sendSuccessToast("Project created successfully!");
+      })
+      .catch((err) => {
+        sendErrorToast("An unexpected error occured!");
+      });
+  };
 
   return (
     <div className="create_project_container">
       <h4>Create new project</h4>
-      <ProjectForm btnText="Add new Project" handleSubmit={createNewProject}/>
+      <ProjectForm btnText="Add new Project" handleSubmit={createNewProject} />
     </div>
-  )
+  );
 }
