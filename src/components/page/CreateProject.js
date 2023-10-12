5a6eb362decfe783fe0ddfe76b569b7835edc3a8
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-10-12 14:59:58 -0300

    Get categories and map them into select options

--- src/components/page/CreateProject.js
+++ src/components/page/CreateProject.js
@@ -1,8 +1,20 @@
 import { Button, Form } from "react-bootstrap";
 import "./CreateProject.modules.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { useEffect, useState } from "react";
+import baseConnection from "../../config/baseConnection";
 
 export default function CreateProject() {
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    baseConnection.get('/categories').then((response) => {
+      setCategories(response.data);
+    });
+}, []);
+
+
   return (
     <div className="create_project_container">
       <h4>Create new project</h4>
@@ -15,9 +27,9 @@ export default function CreateProject() {
         </Form.Group>
         <Form.Select className="mb-4">
           <option>Select a category</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
+          {categories.map((option) => {
+            return <option value={option.id} key={option.id}>{option.name}</option>
+          })}
         </Form.Select>
         <Button variant="dark" type="submit">
           Add new Project
