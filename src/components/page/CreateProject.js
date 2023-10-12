42f5c2956bd5540c10c52f42db70957bc73aaa42
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-10-12 12:12:40 -0300

    Add form for new project page

--- src/components/page/CreateProject.js
+++ src/components/page/CreateProject.js
@@ -1,3 +1,28 @@
+import { Button, Form } from "react-bootstrap";
+import "./CreateProject.modules.css";
+import "bootstrap/dist/css/bootstrap.css";
+
 export default function CreateProject() {
-    return (<><p>PLACEHOLDER</p></>)
+  return (
+    <div className="create_project_container">
+      <h4>Create new project</h4>
+      <Form>
+        <Form.Group className="mb-4">
+          <Form.Control type="text" placeholder="Enter the project name" />
+        </Form.Group>
+        <Form.Group className="mb-4">
+          <Form.Control type="text" placeholder="Enter a maximum budget" />
+        </Form.Group>
+        <Form.Select className="mb-4">
+          <option>Select a category</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+        <Button variant="dark" type="submit">
+          Add new Project
+        </Button>
+      </Form>
+    </div>
+  )
 }
